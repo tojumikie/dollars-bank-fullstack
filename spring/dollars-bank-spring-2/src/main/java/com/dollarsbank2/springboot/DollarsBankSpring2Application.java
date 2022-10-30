c6f6b2056f9383884d2cf3b489ae5b5ef332f82b
Toju Mikie <40072570+tojumikie@users.noreply.github.com>
2022-10-30 12:46:50 -0400

    created new version of dollars bank application
    -I may or may not run into another roadblock here. I need to have working log in and log out

---
+++ spring/dollars-bank-spring-2/src/main/java/com/dollarsbank2/springboot/DollarsBankSpring2Application.java
@@ -0,0 +1,13 @@
+package com.dollarsbank2.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsBankSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsBankSpring2Application.class, args);
+	}
+
+}
---
+++ spring/dollars-bank-spring-2/src/test/java/com/dollarsbank2/springboot/DollarsBankSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollarsbank2.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarsBankSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
