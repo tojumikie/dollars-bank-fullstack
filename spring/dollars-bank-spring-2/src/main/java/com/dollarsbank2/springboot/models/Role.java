07c1cda12d2116a4be677c7c3c352fdb3ab2487d
Toju Mikie <40072570+tojumikie@users.noreply.github.com>
2022-10-30 18:01:48 -0400

    created role file for hibernate
    -this will create a table automatically in mysql

---
+++ spring/dollars-bank-spring-2/src/main/java/com/dollarsbank2/springboot/models/Role.java
@@ -0,0 +1,42 @@
+package com.dollarsbank2.springboot.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(length = 30)
+	private RoleEnum name;
+	public Role() {
+		
+	}
+	public Role(RoleEnum name) {
+//		super();
+		this.name = name;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public RoleEnum getName() {
+		return name;
+	}
+	public void setName(RoleEnum name) {
+		this.name = name;
+	}
+	
+}
---
+++ spring/dollars-bank-spring-2/src/main/java/com/dollarsbank2/springboot/models/RoleEnum.java
@@ -0,0 +1,7 @@
+package com.dollarsbank2.springboot.models;
+
+public enum RoleEnum {
+	ROLE_USER,
+	ROLE_MODERATOR,
+	ROLE_ADMIN
+}
